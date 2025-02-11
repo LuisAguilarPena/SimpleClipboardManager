075458ca5b9a0572b731d6a8f49638d5fa26d993
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2025-02-11 11:21:00 -0600

    changes to main to allow for node require

--- electron/main.ts
+++ electron/main.ts
@@ -30,6 +30,9 @@ function createWindow() {
   win = new BrowserWindow({
     icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
     webPreferences: {
+      // needed for require
+      nodeIntegration: true,
+      contextIsolation: false,
       preload: path.join(__dirname, 'preload.mjs'),
     },
   })
