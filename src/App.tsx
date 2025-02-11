e5a4e77eacf27235d4986d7f5e7af45b3342efc2
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2025-02-10 21:15:57 -0600

    added basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,161 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
-import './App.css'
+import './bulma.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className='section title-section'>
+        <div className="container">
+          <h1 className="title is-1">Clipboard Manager</h1>
+          <h2 className="subtitle is-3">Review your clipboard history below</h2>
+        </div>
+      </section>
+      <section className='section clipboard-section'>
+        <div className="container">
+          <div className="columns is-multiline">
+            <div className="column is-3">
+              <article className="message is-" style={{ height: '320px', overflow: 'auto' }}>
+                <div className="message-header">
+                  <p>#1</p>
+                  <div className='is-flex is-align-items-center'>
+                    <button className="delete copy" style={{transform: 'rotate(-45deg)'}}></button>
+                    <button className="delete"></button>
+                  </div>
+                </div>
+                <div className="message-body" style={{height: '258px'}}>
+                  Green technology and climate change we need to get the vernacular right, so waste of resources. Deliverables feature creep, but through the lens of, so circle back. Peel the onion idea shower, nor powerpoint Bunny, core competencies, or cross pollination across our domains cross functional teams enable out of the box brainstorming. Open door policy i called the it department about that ransomware because of the old antivirus, but he said that we were using avast 2021 a tentative event rundown is attached for your reference, including other happenings on the day you are most welcome.
+                </div>
+              </article>
+            </div>
+            <div className="column is-3">
+              <article className="message is-" style={{ height: '320px', overflow: 'auto' }}>
+                <div className="message-header">
+                  <p>#2</p>
+                  <div className='is-flex is-align-items-center'>
+                    <button className="delete copy" style={{transform: 'rotate(-45deg)'}}></button>
+                    <button className="delete"></button>
+                  </div>
+                </div>
+                <div className="message-body" style={{height: '258px'}}>
+                  Green technology and climate change we need to get the vernacular right, so waste of resources. Deliverables feature creep, but through the lens of, so circle back. Peel the onion idea shower, nor powerpoint Bunny, core competencies, or cross pollination across our domains cross functional teams enable out of the box brainstorming. Open door policy i called the it department about that ransomware because of the old antivirus, but he said that we were using avast 2021 a tentative event rundown is attached for your reference, including other happenings on the day you are most welcome.
+                </div>
+              </article>
+            </div>
+            <div className="column is-3">
+              <article className="message is-" style={{ height: '320px', overflow: 'auto' }}>
+                <div className="message-header">
+                  <p>#3</p>
+                  <div className='is-flex is-align-items-center'>
+                    <button className="delete copy" style={{transform: 'rotate(-45deg)'}}></button>
+                    <button className="delete"></button>
+                  </div>
+                </div>
+                <div className="message-body" style={{height: '258px'}}>
+                  Green technology and climate change we need to get the vernacular 
+                </div>
+              </article>
+            </div>
+            <div className="column is-3">
+              <article className="message is-" style={{ height: '320px', overflow: 'auto' }}>
+                <div className="message-header">
+                  <p>#4</p>
+                  <div className='is-flex is-align-items-center'>
+                    <button className="delete copy" style={{transform: 'rotate(-45deg)'}}></button>
+                    <button className="delete"></button>
+                  </div>
+                </div>
+                <div className="message-body" style={{height: '258px'}}>
+                  Green technology and climate change we need to get the vernacular right, so waste of resources. Deliverables feature creep, but through the lens of, so circle back. Peel the onion idea shower, nor powerpoint Bunny, core competencies, or cross pollination across our domains cross functional teams enab
+                </div>
+              </article>
+            </div>
+            <div className="column is-3">
+              <article className="message is-" style={{ height: '320px', overflow: 'auto' }}>
+                <div className="message-header">
+                  <p>#5</p>
+                  <div className='is-flex is-align-items-center'>
+                    <button className="delete copy" style={{transform: 'rotate(-45deg)'}}></button>
+                    <button className="delete"></button>
+                  </div>
+                </div>
+                <div className="message-body" style={{height: '258px'}}>
+                  Green technology and climate change we need to get the vernacular right, so waste of resources. Deliverables feature creep, but through the lens of, so circle back. Peel the onion idea shower, nor powerpoint Bunny, core competencies, or cross pollination across our domains cross functional teams enab
+                </div>
+              </article>
+            </div>
+            <div className="column is-3">
+              <article className="message is-" style={{ height: '320px', overflow: 'auto' }}>
+                <div className="message-header">
+                  <p>#6</p>
+                  <div className='is-flex is-align-items-center'>
+                    <button className="delete copy" style={{transform: 'rotate(-45deg)'}}></button>
+                    <button className="delete"></button>
+                  </div>
+                </div>
+                <div className="message-body" style={{height: '258px'}}>
+                  Green technology and climate change we need to get the vernacular right, so waste of resources. Deliverables feature creep, but through the lens of, so circle back. Peel
+                </div>
+              </article>
+            </div>
+            <div className="column is-3">
+              <article className="message is-" style={{ height: '320px', overflow: 'auto' }}>
+                <div className="message-header">
+                  <p>#7</p>
+                  <div className='is-flex is-align-items-center'>
+                    <button className="delete copy" style={{transform: 'rotate(-45deg)'}}></button>
+                    <button className="delete"></button>
+                  </div>
+                </div>
+                <div className="message-body" style={{height: '258px'}}>
+                  Green technology and climate change we need to get the vernacular right, so waste of resources. Deliverables feature creep, but through the lens of, so circle back. Peel
+                </div>
+              </article>
+            </div>
+            <div className="column is-3">
+              <article className="message is-" style={{ height: '320px', overflow: 'auto' }}>
+                <div className="message-header">
+                  <p>#8</p>
+                  <div className='is-flex is-align-items-center'>
+                    <button className="delete copy" style={{transform: 'rotate(-45deg)'}}></button>
+                    <button className="delete"></button>
+                  </div>
+                </div>
+                <div className="message-body" style={{height: '258px'}}>
+                  Green technology and climate change we need to get the vernacular right, so waste of resources. Deliverables feature creep, but through the lens of, so circle back. Peel
+                </div>
+              </article>
+            </div>
+            <div className="column is-3">
+              <article className="message is-" style={{ height: '320px', overflow: 'auto' }}>
+                <div className="message-header">
+                  <p>#9</p>
+                  <div className='is-flex is-align-items-center'>
+                    <button className="delete copy" style={{transform: 'rotate(-45deg)'}}></button>
+                    <button className="delete"></button>
+                  </div>
+                </div>
+                <div className="message-body" style={{height: '258px'}}>
+                  Green technology and climate change we need to get the vernacular right, so waste of resources. Deliverables feature creep, but through the lens of, so circle back. Peel
+                </div>
+              </article>
+            </div>
+            <div className="column is-3">
+              <article className="message is-" style={{ height: '320px', overflow: 'auto' }}>
+                <div className="message-header">
+                  <p>#10</p>
+                  <div className='is-flex is-align-items-center'>
+                    <button className="delete copy" style={{transform: 'rotate(-45deg)'}}></button>
+                    <button className="delete"></button>
+                  </div>
+                </div>
+                <div className="message-body" style={{height: '258px'}}>
+                  Green technology and climate change we need to get the vernacular right, so waste of resources. Deliverables feature creep, but through the lens of, so circle back. Peel
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section >
     </>
   )
 }
